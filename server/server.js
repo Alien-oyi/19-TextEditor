73e9a17717870962917463392135c8d6f1bd6286
Alien-oyi <daijinmin78@gmail.com>
2023-04-06 19:24:25 -0400

    set up server.js

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// app.use(express.static('../client/dist'));
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+
+// require('./routes/htmlRoutes')(app);
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
