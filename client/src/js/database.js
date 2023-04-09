0c912169144258b57df3f5c0602036b54e60a44e
Alien-oyi <daijinmin78@gmail.com>
2023-04-09 16:49:40 -0400

    fix getDb bug

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -17,7 +17,7 @@ export const putDb = async (content) => {
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.add({ content: content });
+  const request = store.put({ content: content });
   const result = await request;
   console.log('data saved', result);
 };
@@ -31,7 +31,7 @@ export const getDb = async () => {
   const request = store.getAll(); 
   const result = await request;
   console.log('result.value', result);
-  return result;
+  return result.value;
 };
 
 initdb();
